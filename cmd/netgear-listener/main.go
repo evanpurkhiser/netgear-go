40273b00b0ea2ee3781521753862886180d04c1d
Evan Purkhiser <evanpurkhiser@gmail.com>
2016-09-15 23:54:39 -0700

    Move listener method out of main

--- cmd/netgear-listener/main.go
+++ cmd/netgear-listener/main.go
@@ -19,19 +19,19 @@ var output = map[netgear.DeviceChange]string{
 	netgear.DeviceRemoved: "Device Removed",
 }
 
+func listener(change *netgear.ChangedDevice, err error) {
+	if err != nil {
+		return
+	}
+
+	mac := change.Device.MAC.String()
+	fmt.Printf(output[change.Change]+": %s\n", mac)
+}
+
 func main() {
 	flag.Parse()
 
 	client := netgear.NewClient(*host, *username, *password)
-
-	listener := func(change *netgear.ChangedDevice, err error) {
-		if err != nil {
-			return
-		}
-
-		mac := change.Device.MAC.String()
-		fmt.Printf(output[change.Change]+": %s\n", mac)
-	}
 
 	pollTime := time.Second * 10
 	client.OnDeviceChanged(pollTime, listener)
